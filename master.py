bea57f6260934572708eae4ed7efee5aa95f604e
Michal Damski <damski.michal@gmail.com>
2022-12-29 22:24:41 +0100

    Drugi commit

--- master.py
+++ master.py
@@ -1,7 +1,14 @@
 import requests
+import json
 link = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=EUR"
 odbior = requests.get(link)
 odbior.status_code
+print (odbior.status_code)
+parameters = []
+krypto = input ("Podaj skrot krypto do wyfiltrowania:")
+krypto = krypto.upper()
+parameters.append(krypto)
 odbior.text
 odbior.json()
-print (odbior.status_code)
+print (parameters)
+#print (odbior.json())
