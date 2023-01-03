42dfc2701945f9088c15dbca310f7d628075d922
Michal Damski <damski.michal@gmail.com>
2023-01-03 22:10:49 +0100

    dodany pandas, typowanie danych, próba zapisu w df

--- master.py
+++ master.py
@@ -1,14 +1,17 @@
 import requests
+import pandas as pd
 import json
 
-url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=EUR"
+url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=EUR"
 response = requests.get(url)
 big_data = requests.get(url).json()
 response.text
 open("response.json", "wb").write(response.content)
-
+print("Witamy w CryptoCheckerze!")
 #SPRAWDZANIE STATUSU API
-#print (response['Type'])
+print("--------------------")
+print(f"Sprawdzenie odpowiedzi serwera danych: {response.status_code}")
+print("--------------------")
 
 #PARAMETRY WYSZUKIWANIA
 #parameters = []
@@ -17,19 +20,40 @@ open("response.json", "wb").write(response.content)
 #parameters.append(krypto)
 #print (parameters)
 
+#TWORZĘ SZABLON DANYCH
+
+df = pd.DataFrame(columns=['item_fullname','item_name','item_price','item_lowday','item_highday','item_entrydate'])
+
+#PĘTLA WYCIĄGAJĄCA DANE O KRYPTOWALUTACH Z OBIEKTÓW W API
 
 for item in big_data['Data']:
-    item_name = item['CoinInfo']['Name']
     item_fullname = item['CoinInfo']['FullName']
-    item_entrydate = item['CoinInfo']['AssetLaunchDate']
+    item_fullname = str(item_fullname)
+    item_name = item['CoinInfo']['Name']
+    item_name = str(item_name)
     item_price = item['DISPLAY']['EUR']['PRICE']
-    item_highday = item['DISPLAY']['EUR']['HIGHDAY']
+    item_price = item_price.replace(",", "")
+    item_price = item_price.replace("€ ","")
+    item_price = float(item_price)
     item_lowday = item['DISPLAY']['EUR']['LOWDAY']
-    print(item_name)
-    print(item_fullname)
-    print(item_price)
-    print(item_highday)
-    print(item_lowday)
-    print(item_entrydate)
-    print("Nowe krypto")
-#print (big_data['Data'][0])
+    item_lowday = item_lowday.replace(",","")
+    item_lowday = item_lowday.replace("€ ","")
+    item_lowday = float(item_lowday)
+    item_highday = item['DISPLAY']['EUR']['HIGHDAY']
+    item_highday = item_highday.replace(",","")
+    item_highday = item_highday.replace("€ ","")
+    item_highday = float(item_highday)
+    item_entrydate = item['CoinInfo']['AssetLaunchDate']
+    item_entrydate = str(item_entrydate)
+    df1 = pd.DataFrame({'item_fullname':item_fullname, 'item_name':item_name, 'item_price':item_price, 'item_lowday':item_lowday, 'item_highday':item_highday, 'item_entrydate':item_entrydate},ignore_index=True)
+    df = pd.concat([df,df1])
+    print(f"Nazwa kryptowaluty: {item_fullname}")
+    print(f"Oznaczenie kryptowaluty: {item_name}")
+    print(f"Aktualna cena: {item_price}")
+    print(f"Najnizsza cena dzisiaj: {item_lowday}")
+    print(f"Najwyzsza cena dzisiaj: {item_highday}")
+    print(f"Data wejscia na rynek kryptowalut: {item_entrydate}")
+    print("--------------------")
+    print("Następna kryptowaluta")
+    print("--------------------")
+df
